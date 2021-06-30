3f7c0934a44a5efb4669a22c7b344057c8423372
nico <liuqianyuan@bjx.cloud>
2021-06-30 13:44:31 +0800

    merge bench test.

--- hashmap_bench_test.go
+++ hashmap_bench_test.go
@@ -1,20 +1,43 @@
-/**
-2 * @Author: Nico
-3 * @Date: 2021/6/28 21:40
-4 */
 package hashmap
 
 import (
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"testing"
 )
 
-var SHARD_COUNT = 2
+const benchmarkItemCount = 1 << 10 // 1024
 
-type Animal struct {
-	name string
+func setupHashMap(b *testing.B) *HashMap {
+	m := New()
+	for i := uintptr(0); i < benchmarkItemCount; i++ {
+		m.Set(i, i)
+	}
+	b.ResetTimer()
+	return m
 }
+
+func setupGoMap(b *testing.B) map[uintptr]uintptr {
+	m := make(map[uintptr]uintptr)
+	for i := uintptr(0); i < benchmarkItemCount; i++ {
+		m[i] = i
+	}
+
+	b.ResetTimer()
+	return m
+}
+
+func setupGoSyncMap(b *testing.B) *sync.Map {
+	m := &sync.Map{}
+	for i :=uintptr(0); i < benchmarkItemCount; i++ {
+		m.Store(i, i)
+	}
+
+	b.ResetTimer()
+	return m
+}
+
 
 func BenchmarkStrconv(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -205,16 +228,16 @@ func BenchmarkMultiGetSetBlockSyncMap(b *testing.B) {
 
 func GetSet(m *HashMap, finished chan struct{}) (set func(key, value string), get func(key, value string)) {
 	return func(key, value string) {
-		for i := 0; i < 10; i++ {
-			m.Get(key)
+			for i := 0; i < 10; i++ {
+				m.Get(key)
+			}
+			finished <- struct{}{}
+		}, func(key, value string) {
+			for i := 0; i < 10; i++ {
+				m.Set(key, value)
+			}
+			finished <- struct{}{}
 		}
-		finished <- struct{}{}
-	}, func(key, value string) {
-		for i := 0; i < 10; i++ {
-			m.Set(key, value)
-		}
-		finished <- struct{}{}
-	}
 }
 
 func GetSetSyncMap(m *sync.Map, finished chan struct{}) (get func(key, value string), set func(key, value string)) {
@@ -233,3 +256,187 @@ func GetSetSyncMap(m *sync.Map, finished chan struct{}) (get func(key, value str
 	return
 }
 
+func BenchmarkReadHashMapUint(b *testing.B) {
+	m := setupHashMap(b)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := uintptr(0); i < benchmarkItemCount; i++ {
+				j, _ := m.Get(i)
+				if j != i {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadGoMapUintUnsafe(b *testing.B) {
+	m := setupGoMap(b)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i :=uintptr(0); i < benchmarkItemCount; i++ {
+				j := m[i]
+				if j != i {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadGoMapUintMutex(b *testing.B) {
+	m := setupGoMap(b)
+	l := &sync.RWMutex{}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i :=uintptr(0); i < benchmarkItemCount; i++ {
+				l.RLock()
+				j := m[i]
+				l.RUnlock()
+				if j != i {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadGoSyncMapUint(b *testing.B) {
+	m := setupGoSyncMap(b)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i :=uintptr(0); i < benchmarkItemCount; i++ {
+				j, _ := m.Load(i)
+				if j != i {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadHashMapWithWritesUint(b *testing.B) {
+	m := setupHashMap(b)
+	var writer int32
+
+	b.RunParallel(func(pb *testing.PB) {
+		// use 1 thread as writer
+		if atomic.CompareAndSwapInt32(&writer, 0, 1) {
+			for pb.Next() {
+				for i :=uintptr(0); i < benchmarkItemCount; i++ {
+					m.Set(i, i)
+				}
+			}
+		} else {
+			for pb.Next() {
+				for i :=uintptr(0); i < benchmarkItemCount; i++ {
+					j, _ := m.Get(i)
+					if j != i {
+						b.Fail()
+					}
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadGoMapWithWritesUintMutex(b *testing.B) {
+	m := setupGoMap(b)
+	l := &sync.RWMutex{}
+	var writer int32
+
+	b.RunParallel(func(pb *testing.PB) {
+		// use 1 thread as writer
+		if atomic.CompareAndSwapInt32(&writer, 0, 1) {
+			for pb.Next() {
+				for i :=uintptr(0); i < benchmarkItemCount; i++ {
+					l.Lock()
+					m[i] = i
+					l.Unlock()
+				}
+			}
+		} else {
+			for pb.Next() {
+				for i :=uintptr(0); i < benchmarkItemCount; i++ {
+					l.RLock()
+					j := m[i]
+					l.RUnlock()
+					if j != i {
+						b.Fail()
+					}
+				}
+			}
+		}
+	})
+}
+
+
+func BenchmarkReadGoSyncMapWithWritesUint(b *testing.B) {
+	m := setupGoSyncMap(b)
+	var writer int32
+
+	b.RunParallel(func(pb *testing.PB) {
+		// use 1 thread as writer
+		if atomic.CompareAndSwapInt32(&writer, 0, 1) {
+			for pb.Next() {
+				for i :=uintptr(0); i < benchmarkItemCount; i++ {
+					m.Store(i, i)
+				}
+			}
+		} else {
+			for pb.Next() {
+				for i :=uintptr(0); i < benchmarkItemCount; i++ {
+					j, _ := m.Load(i)
+					if j != i {
+						b.Fail()
+					}
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkWriteHashMapUint(b *testing.B) {
+	m := New()
+
+	for n := 0; n < b.N; n++ {
+		for i :=uintptr(0); i < benchmarkItemCount; i++ {
+			m.Set(i, i)
+		}
+	}
+}
+
+func BenchmarkWriteGoMapUnsafeUint(b *testing.B) {
+	m := make(map[uintptr]uintptr)
+
+	for n := 0; n < b.N; n++ {
+		for i :=uintptr(0); i < benchmarkItemCount; i++ {
+			m[i] = i
+		}
+	}
+}
+
+
+func BenchmarkWriteGoMapMutexUint(b *testing.B) {
+	m := make(map[uintptr]uintptr)
+	l := &sync.RWMutex{}
+
+	for n := 0; n < b.N; n++ {
+		for i :=uintptr(0); i < benchmarkItemCount; i++ {
+			l.Lock()
+			m[i] = i
+			l.Unlock()
+		}
+	}
+}
+
+func BenchmarkWriteGoSyncMapUint(b *testing.B) {
+	m := &sync.Map{}
+
+	for n := 0; n < b.N; n++ {
+		for i :=uintptr(0); i < benchmarkItemCount; i++ {
+			m.Store(i, i)
+		}
+	}
+}
